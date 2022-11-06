cc63f20b5465a4fb3329b0b323de653e94fd43e4
Felipe Monteiro <felipedeamonteiro@gmail.com>
2022-11-06 00:01:13 -0300

    Add tests and fixed them

--- src/__tests__/pages/index.test.tsx
+++ src/__tests__/pages/index.test.tsx
@@ -1,37 +1,101 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import Home from '../../pages/index';
 import '@testing-library/jest-dom';
-import MockAdapter from 'axios-mock-adapter';
-import api from '../../services/api';
+import '@testing-library/jest-dom/extend-expect';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 
-const apiMock = new MockAdapter(api);
+const apiResponse1 = [
+  {
+    address: "Kulas Light, Apt. 556 - 92998-3874 Gwenborough",
+    company: "Romaguera-Crona",
+    email: "Sincere@april.biz",
+    id: 1,
+    name: "Leanne Graham",
+    phone: "1-770-736-8031 x56442",
+    username: "Bret",
+    website: "hildegard.org"
+  }
+];
+
+const apiResponse2 = [
+  {
+    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+    id: 1,
+    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
+  }
+];
+
+const userResponse = rest.get('https://jsonplaceholder.typicode.com/users', (req, res, ctx) => {
+  return res(ctx.json(apiResponse1));
+});
+
+const postResponse = rest.get('https://jsonplaceholder.typicode.com/posts', (req, res, ctx) => {
+  return res(ctx.json(apiResponse2));
+});
+
+
+const handler = [userResponse, postResponse];
+
+const server = setupServer(...handler);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe('Home', () => {
   it('should render the Homepage', () => {
-    const { container } = render(<Home />)
-    expect(container).toMatchSnapshot()
+    const { container } = render(<Home />);
+    expect(container).toMatchSnapshot();
   });
 
-  it('should be able to fetch data and show on the screen', () => {
-    const apiResponse = {
-      id: 1,
-      name: "Leanne Graham",
-      username: "Bret",
-      email: "Sincere@april.biz",
-      address: {
-        street: "Kulas Light",
-        suite: "Apt. 556",
-        city: "Gwenborough",
-        zipcode: "92998-3874",
-        geo: {
-          lat: "-37.3159",
-          lng: "81.1496"
-        }
-      },
-    };
+  it('shouldn\'t be able to fetch data and show error on the screen', async () => {
 
-    apiMock.onGet('users').reply(200, apiResponse);
+    server.use(
+      rest.get('https://jsonplaceholder.typicode.com/users', (req, res, ctx) => {
+        return res(ctx.status(500))
+      }),
+    )
+    const { getByText } = render(<Home />);
+    const buttonElement = getByText('Click to get data');
+    expect(buttonElement).toBeTruthy();
 
+    fireEvent.click(buttonElement);
+
+    const errorMessage = await waitFor(() => getByText('Reload page and try again'));
+    
+    expect(errorMessage).toBeTruthy();    
+  });
+
+  it('should be able to fetch data and show new button text on the screen', async () => {
+
+    const { getByText } = render(<Home />);
+    const buttonElement = getByText('Click to get data');
+    expect(buttonElement).toBeTruthy();
+
+    fireEvent.click(buttonElement);
+
+    const buttonElement2 = await waitFor(() => getByText('Click to remove data'));
+    
+    expect(buttonElement2).toBeTruthy();    
+  });
+
+  it('should be able to fetch data and show data on the screen. When button is clicked again, the data should be removed', async () => {
+
+    const { getByText } = render(<Home />);
+    const buttonElement = getByText('Click to get data');
+    expect(buttonElement).toBeTruthy();
+
+    fireEvent.click(buttonElement);
+
+    const buttonElement2 = await waitFor(() => getByText('Click to remove data'));
+    
+    expect(buttonElement2).toBeTruthy();
+
+    fireEvent.click(buttonElement2);
+
+    await waitFor(() => getByText('Click to get data'));
+    expect(buttonElement).toBeTruthy();
   });
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -87,68 +87,74 @@ const Container = styled('div', {
 
   '.text-data': {
     color: '$greenBlue'
+  },
+
+  '.error-message': {
+    color: 'red',
+    fontSIze: '18px',
+    fontWeight: 'bold',
+    marginTop: '50px'
+  },
+
+  '.loading': {
+    color: '$greenBlue',
+    marginTop: '50px'
   }
 })
 
 export default function Home() {
-  const [users, setUsers] = useState<usersData[]>([]);
-  const [posts, setPosts] = useState<postsData[]>([]);
-  const [errors, setErrors] = useState<string[]>([]);
+  const [errors, setErrors] = useState<string>('');
   const [showErrors, setShowErrors] = useState<boolean>(false);
   const [letter, setLetter] = useState<lettersData[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    const loadData = async () => {
-      try {
-        const usersResponse = await api.get('users');
-        const postsResponse = await api.get('posts');
-  
-        setUsers(usersResponse.data);
-        setPosts(postsResponse.data);
-      } catch (err) {
-        setErrors(err);
-      }
-    }
-    loadData();
-  }, []);
-
-  const getLetter = () => {
-    if (errors.length > 0) {
-      setShowErrors(true);
-      return;
-    }
-
-    const letterObject: lettersData[] = users.map((user: usersData) => {
-      const allUserPosts = posts.filter((post: postsData) => post.userId === user.id);
-      const allValidPostsData = allUserPosts.map((userPost: postsData) => {
-        return {
-          id: userPost.id,
-          title: userPost.title,
-          body: userPost.body
-        }
-      });
-      
-      return {
-        id: user.id,
-        name: user.name,
-        username: user.username,
-        email: user.email,
-        address: `${user.address.street}, ${user.address.suite} - ${user.address.zipcode} ${user.address.city}`,
-        phone: user.phone,
-        website: user.website,
-        company: user.company.name,
-        posts: [...allValidPostsData]
-      }
-    });
+  const getLetter = async () => {
+    setShowErrors(false);
+    setErrors('');
 
     if (letter.length > 0) {
       setLetter([]);
+      setLoading(false);
     } else {
-      setLetter(letterObject);
-    }
+      setLoading(true);
+      try {
+        const { data: usersResponse } = await api.get('users');
+        const { data: postsResponse } = await api.get('posts');
+  
+        const letterObject: lettersData[] = usersResponse.map((user: usersData) => {
+          const allUserPosts = postsResponse.filter((post: postsData) => post.userId === user.id);
+          const allValidPostsData = allUserPosts.map((userPost: postsData) => {
+            return {
+              id: userPost.id,
+              title: userPost.title,
+              body: userPost.body
+            }
+          });
+          
+          return {
+            id: user.id,
+            name: user.name,
+            username: user.username,
+            email: user.email,
+            address: `${user.address.street}, ${user.address.suite} - ${user.address.zipcode} ${user.address.city}`,
+            phone: user.phone,
+            website: user.website,
+            company: user.company.name,
+            posts: [...allValidPostsData]
+          }
+        });
 
-    
-  }
+        setLetter(letterObject);
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+        setErrors(err.message);
+        setShowErrors(true);
+      }; 
+      
+    } 
+     
+  };
 
   return (
     <Container>
@@ -159,14 +165,23 @@ export default function Home() {
         'Click to get data'
         }
       </Button>
-      
-      {errors.length > 0 
-        ? <div>Error</div> 
+      {loading
+      ? (
+        <h3 className="loading">Loading...</h3>
+      )
+      : (
+        showErrors 
+        ? (
+          <>
+            <div className="error-message">{errors}</div>
+            <div className="error-message">Reload page and try again</div>
+          </>
+          ) 
         : (letter.length > 0 
           ? (<Slider data={letter} />)
           : ''
         )
-      }
+      )}
     </Container>
   )
 }
